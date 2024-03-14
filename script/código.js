10c1870f2df0c29772af3c45936aa6f383f149de
Ivan de Jesus Franco Lima <ivanfrancolima@hotmail.com>
2024-03-14 18:23:49 -0300

    ajustes pontuais

--- script/código.js
+++ script/código.js
@@ -1,12 +1,24 @@
 var icone = window.document.querySelector('#hamburguer')
 var menu = window.document.querySelector('#caminhos')
 
+
+function tamanho_de_tela(){
+    if(window.innerWidth>=970){
+        icone.style.display='none';
+        menu.style.display='block';
+    }else{
+        icone.style.display='block';
+        menu.style.display='none';
+    }
+}
+
 function tamanho_tela(){
     if(window.innerWidth>=970){
         icone.style.display='none';
-        menu.style.display='block'
+        menu.style.display='block';
     }else{
         icone.style.display='block';
+        menu.style.display='none';
     }
 }
 
